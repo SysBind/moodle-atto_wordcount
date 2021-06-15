31a846db5dbb624f2106b353640432f3b9b80154
avi <avi@sysbind.co.il>
2021-06-16 02:32:34 +0300

    atto_wordcount: Create first release

--- version.php
+++ version.php
@@ -27,5 +27,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'atto_wordcount';
 $plugin->version = 2021061500;
 $plugin->requires = '2018051718';
-$plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.0.1';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0.0';
