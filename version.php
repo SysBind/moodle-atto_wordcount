8151146868da1c4db654e34436020b8fe34ec717
Avi Levy <avi@sysbind.co.il>
2021-06-16 07:14:30 +0300

    Bump release

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2021061600;
 $plugin->requires = 2020061500;
 $plugin->component = 'atto_wordcount';
-$plugin->release = '1.0.4';
+$plugin->release = '1.0.6';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [39, 310, 311];
 $plugin->incompatible = 38;
