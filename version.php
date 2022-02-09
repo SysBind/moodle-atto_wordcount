252b2022fd4c8fc0d23bf5bd78b7f307dc96fb6b
Menrath André <andre.menrath@uni-graz.at>
2022-02-09 13:02:21 +0100

    version increase

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022020900;
+$plugin->version = 2022020901;
 $plugin->requires = 2014051200;
 $plugin->component = 'atto_wordcount';
 $plugin->release = '1.2.0';
