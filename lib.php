290b2dcabcc35c0194e8ef510062b8df616af416
Menrath André <andre.menrath@uni-graz.at>
2022-02-25 10:10:44 +0100

    remove unused and deprecated strings_for_js

--- lib.php
+++ lib.php
@@ -26,15 +26,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 /**
- * Initialise the js strings required for this module.
- */
-function atto_wordcount_strings_for_js() {
-    global $PAGE;
-
-    $PAGE->requires->strings_for_js(['words', 'wordscount'], 'atto_wordcount');
-}
-
-/**
  * Pass the wordlimits to the js part of the plugin.
  */
 function atto_wordcount_params_for_js ($elementid, $options, $foptions) {
